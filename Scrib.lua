05a18481afdd357052e2ecfd921763cb1bbea4be
HilanderGK <154356175+HilanderGK@users.noreply.github.com>
2025-02-15 17:49:46 +0200

    Update Scrib.lua

--- Scrib.lua
+++ Scrib.lua
@@ -1,39 +1,58 @@
+-- Load Rayfield UI Library
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+-- Create Window
 local Window = Rayfield:CreateWindow({
-   Name = "Evohub",
-   LoadingTitle = "Loading...",
-   LoadingSubtitle = "Some Guy",
-   Theme = "Default",
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false,
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil,
-      FileName = "Evohub"
-   },
-
-   Discord = {
-      Enabled = true,
-      Invite = "evohub",
-      RememberJoins = true
-   },
-
-   KeySystem = false
+    Name = "Jujutsu Infinite Hub",
+    LoadingTitle = "Loading...",
+    LoadingSubtitle = "by HilanderGK",
+    Theme = "Default"
 })
 
-local Tab1 = Window:CreateTab("Evohub Invite", 4483362458)
+-- Create Tabs
+local AutoFarmTab = Window:CreateTab("Auto-Farm", 4483362458)
+local TeleportTab = Window:CreateTab("Teleport", 4483362458)
 
-Tab1:CreateButton({
-   Name = "Evohub Invite",
-   Callback = function()
-      setclipboard("https://discord.gg/evohub")
-      Rayfield:Notify({
-         Title = "Discord",
-         Content = "Discord Invite has been copied to your Clipboard!",
-         Duration = 15,
-         Image = 4483362458
-      })
-   end
+-- Auto-Farm Toggle
+local AutoFarm = false
+AutoFarmTab:CreateToggle({
+    Name = "Enable Auto-Farm",
+    CurrentValue = false,
+    Callback = function(value)
+        AutoFarm = value
+        while AutoFarm do
+            pcall(function()
+                for _, enemy in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+                    if enemy:FindFirstChild("HumanoidRootPart") then
+                        local player = game.Players.LocalPlayer
+                        player.Character.HumanoidRootPart.CFrame = enemy.HumanoidRootPart.CFrame * CFrame.new(0, 0, -3)
+                        game:GetService("ReplicatedStorage").Remotes.Attack:FireServer(enemy)
+                    end
+                end
+            end)
+            wait(0.5)
+        end
+    end
+})
+
+-- Teleport to Locations
+TeleportTab:CreateButton({
+    Name = "Teleport to Training Area",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(100, 50, 200) -- Change XYZ based on location
+    end
+})
+
+TeleportTab:CreateButton({
+    Name = "Teleport to Boss",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(500, 60, 1000) -- Change XYZ based on boss location
+    end
+})
+
+-- Notification
+Rayfield:Notify({
+    Title = "Jujutsu Infinite Hub",
+    Content = "Script Loaded Successfully!",
+    Duration = 5
 })
